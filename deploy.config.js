18aece2033c1bf0eaf0411ff0e6f33b917740940
huangjinxing <731727479@qq.com>
2023-09-27 17:57:59 +0800

    代码更新

---
+++ deploy.config.js
@@ -0,0 +1,26 @@
+module.export = {
+  app: [
+    {
+      name: 'nuxt_koa2_blogs',// 项目名称
+      script: 'server/index.js', // 程序执行入口文件
+      env: {
+        COMMON_VARIABLE: 'true'
+      },
+      evn_production: {
+        NODE_ENV: 'production'
+      }
+    },
+  ],
+  deploy: {
+    production: {
+      user: 'rout',
+      host: '119.91.102.168',
+      ref: 'origin/main', // git仓库分支
+      repo: 'https://github.com/HuangJinxing2019/koa-blog.git', // 仓库地址
+      path: '/www/blog', // 部署到服务器的目录地址
+      'pre-deploy': 'git fetch --all',
+      // 下载依赖，项目打包，启动项目，pm2检查是否启动，如未启动重新执行一边
+      'post-deploy': 'npm install && npm run build && npm start && pm2 startOrRestart deploy.config.js --env production'
+    }
+  }
+}
