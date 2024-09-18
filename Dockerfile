7c4d9b9e93f3862613a94a2e90991aeea8ea6286
huangjinxing <731727479@qq.com>
2024-09-18 16:27:02 +0800

    docker部署

---
+++ Dockerfile
@@ -0,0 +1,25 @@
+# 使用官方的 Node.js 镜像
+FROM node:18
+
+# 设置工作目录
+WORKDIR /usr/app
+
+# 复制 package.json 和 package-lock.json 文件
+COPY package*.json ./
+# 设置 node 阿里镜像
+RUN npm config set registry https://npmmirror.com/mirrors/node
+
+# 安装项目依赖
+RUN npm install
+
+# 全局安装 PM2
+RUN npm install -g pm2
+
+# 复制所有项目文件到工作目录
+COPY . .
+
+# 暴露应用的端口
+EXPOSE 3001
+
+# 使用 PM2 启动应用
+CMD ["pm2-runtime", "start", "ecosystem.config.js", "--env", "production"]
--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   app: [
     {
-      name: 'nuxt_koa2_blogs',// 项目名称
+      name: 'nuxt_koa2_blog',// 项目名称
       script: 'server/index.js', // 程序执行入口文件
       env: {
         COMMON_VARIABLE: 'true'
@@ -11,16 +11,16 @@ module.exports = {
       }
     },
   ],
-  deploy: {
-    production: {
-      user: 'root',
-      host: '192.168.1.231',
-      ref: 'origin/main', // git仓库分支
-      repo: 'https://github.com/HuangJinxing2019/koa-blog.git', // 仓库地址
-      path: '/www/blog', // 部署到服务器的目录地址
-      'pre-deploy': 'git fetch --all',
-      // 下载依赖，项目打包，启动项目，pm2检查是否启动，如未启动重新执行一边
-      'post-deploy': 'npm install && npm run build && npm start && pm2 startOrRestart deploy.config.js --env production'
-    }
-  }
+  // deploy: {
+  //   production: {
+  //     user: 'root',
+  //     host: '192.168.1.231',
+  //     ref: 'origin/main', // git仓库分支
+  //     repo: 'https://github.com/HuangJinxing2019/koa-blog.git', // 仓库地址
+  //     path: '/www/blog', // 部署到服务器的目录地址
+  //     'pre-deploy': 'git fetch --all',
+  //     // 下载依赖，项目打包，启动项目，pm2检查是否启动，如未启动重新执行一边
+  //     'post-deploy': 'npm install && npm run build && npm start && pm2 startOrRestart ecosystem.config.js --env production'
+  //   }
+  // }
 }
