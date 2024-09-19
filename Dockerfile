dc38240d0062b07bbdd58d3b7fc7f6bb7670571c
huangjinxing <731727479@qq.com>
2024-09-19 10:06:09 +0800

    docker部署

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,5 @@
 # 使用官方的 Node.js 镜像
-FROM node:18
+FROM node:16.17.1
 
 # 设置工作目录
 WORKDIR /usr/app
--- ecosystem.config.js
+++ ecosystem.config.js
@@ -11,16 +11,16 @@ module.exports = {
       }
     },
   ],
-  // deploy: {
-  //   production: {
-  //     user: 'root',
-  //     host: '192.168.1.231',
-  //     ref: 'origin/main', // git仓库分支
-  //     repo: 'https://github.com/HuangJinxing2019/koa-blog.git', // 仓库地址
-  //     path: '/www/blog', // 部署到服务器的目录地址
-  //     'pre-deploy': 'git fetch --all',
-  //     // 下载依赖，项目打包，启动项目，pm2检查是否启动，如未启动重新执行一边
-  //     'post-deploy': 'npm install && npm run build && npm start && pm2 startOrRestart ecosystem.config.js --env production'
-  //   }
-  // }
+  deploy: {
+    production: {
+      user: 'root',
+      host: '192.168.1.231',
+      ref: 'origin/main', // git仓库分支
+      repo: 'https://github.com/HuangJinxing2019/koa-blog.git', // 仓库地址
+      path: '/www/blog', // 部署到服务器的目录地址
+      'pre-deploy': 'git fetch --all',
+      // 下载依赖，项目打包，启动项目，pm2检查是否启动，如未启动重新执行一边
+      'post-deploy': 'npm install && npm run build && npm start && pm2 startOrRestart ecosystem.config.js --env production'
+    }
+  }
 }
